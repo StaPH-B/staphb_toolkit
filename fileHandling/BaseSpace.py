3fc2988d40184098e33636ff4fd756cac95bb85d
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2018-06-27 01:37:13 -0400

    add CLI functionality

--- fileHandling/BaseSpace.py
+++ fileHandling/BaseSpace.py
@@ -3,6 +3,11 @@
 import os
 import glob
 import subprocess
+import sys
+import argparse
+import datetime
+from time import gmtime, strftime
+
 
 
 class BSProject(object):
@@ -46,10 +51,57 @@ class BSProject(object):
         return
 
     def post_to_basespace(self, report, report_dir):
-        app_results = self.path + "/AppResults/"
-        subprocess.call("cp -R" + " " + report_dir + " " + app_results, shell=True)
+        app_results_dir = self.path + "/AppResults/" + report + strftime("_%Y_%m_%d", gmtime())  + "/"
+        subprocess.call("mkdir" + " " + app_results_dir + " " "&& cp -R" + " " + report_dir + "*" + " "
+                        + app_results_dir, shell=True)
 
-        os.chdir(app_results + report)
+        os.chdir(app_results_dir)
         subprocess.call("basemount-cmd mark-as-complete", shell=True)
 
         print(report_dir + " " + "uploaded to BaseSpace.")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage="BaseSpace.py <input> [options]")
+    parser.add_argument("input", type=str, help="Name of BaseSpace project")
+    parser.add_argument("-o", default="", type=str, help="Name of output_dir. Default will store output within "
+                                                         "an output_dir with the same name as the <input> "
+                                                         "BaseSpace Project")
+    parser.add_argument("-link", action='store_true', help="Will link reads from <input> BaseSpace Project to "
+                                                          "<output_dir>/raw_reads/")
+    parser.add_argument("-postToBS", choices=["Belvidere", "Tredegar"], help="Will post the indicated report to the "
+                                                                             "<input> BaseSpace Project")
+
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+    args = parser.parse_args()
+
+    input_dir = args.input
+    output_dir = args.o
+    link_reads = args.link
+    reportToBS = args.postToBS
+
+    if len(output_dir) > 0:
+        pass
+    else:
+        output_dir = input_dir
+
+    project = BSProject(input_dir)
+
+    print("Selected BaseSpace Project: ", input_dir )
+
+    if link_reads:
+        project.link_reads(output_dir)
+
+    if reportToBS:
+        report_dir = output_dir + "/reports/" + reportToBS + "/"
+
+        if not os.path.isdir(report_dir):
+            raise ValueError("No " + reportToBS + " report was not found.")
+        else:
+            print(report_dir)
+            print(reportToBS)
+            print("Posting", " ", reportToBS, " ", "report to BaseSpace. . .")
+            project.post_to_basespace(str(reportToBS), report_dir)
+
+
