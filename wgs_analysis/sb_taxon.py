9f7aa1503e1e8ae2efc0adc44506d1f067d2dada
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2018-09-04 01:59:30 -0400

    get read data from rawdata dict. rather than raw_read subdir

--- wgs_analysis/sb_taxon.py
+++ wgs_analysis/sb_taxon.py
@@ -1,88 +1,68 @@
 #!/usr/bin/env python3
 
 import os
-import glob
-import re
 import sys
 import argparse
 import subprocess
-from time import gmtime, strftime
+from staphB_ToolKit.fileHandling import sb_read_data
 
 class SBTaxon:
 
-    def __init__(self, project_dir, read_dir = None):
-        self.name = project_dir  # name of wgs project
-        self.path = os.path.expanduser("~") + "/" + self.name  # path to project
-        if read_dir is None:
-            read_dir = "/raw_reads"
-        self.reads = glob.glob(self.path + read_dir + "/*.fastq*")  # Read files
-        test = glob.glob("")
+    def __init__(self, path, output_dir = ""):
+        self.name = path
+        self.reads = sb_read_data.SBReads(self.name)
+        if not output_dir:
+            output_dir = os.getcwd()
+        self.output = output_dir
 
-        if not os.path.isdir(self.name):
-            raise ValueError(self.name + " " + "not found.")
-
-        if len(self.reads) == 0:
-            print(self.path + read_dir)
-            print(self.reads)
+        if len(self.reads.idList) == 0:
             raise ValueError("No read files in" + " " + self.name)
 
-        return
 
-    def sb_mash(self, samples=None):
-        mash_output_dir = self.path + "/mash_output/"
+    def sb_mash(self):
+        mash_output_dir = self.output + "/mash_output/"
+        mash_db = "/opt/genomes/RefSeqSketchesDefaults.msh"
 
         if not os.path.isdir(mash_output_dir):
             os.makedirs(mash_output_dir)
             print("Directory for mash output made: ", mash_output_dir)
 
-        if samples is None:
-            reads = self.reads
-        else:
-            samples = samples.split(", ")
-            reads = list()
-            for sample in samples:
-                reads.extend(glob.glob(self.path + sample + "*.fastq*"))
+        #data dictionary for storing mash species prediction
+        #formatted id: predicted_species
+        mash_species = {}
 
-        r = re.compile(".*_1.fastq")
-        fwd_reads = list(filter(r.match, reads))
+        # run MASH and store top hit in mash_species
+        for id in self.reads.idList:
+            mash_species[id] = ""
 
-        rev_reads = list()
-        for read in fwd_reads:
-            rev_read = read.replace("_1.fastq", "_2.fastq")
-            rev_reads.append(rev_read)
+            r1 = self.reads.rawdata[id][0]
+            r2 = self.reads.rawdata[id][1]
 
-        isolates = []
+            cat_reads = mash_output_dir + id + ".fastq"
+            mash_sketch = mash_output_dir + id + ".fastq.msh"
+            mash_result = mash_output_dir + id + "_distance.tab"
 
-        for read in fwd_reads:
-            File = read
-            File_split = File.split('/')
-            read = File_split[-1]
-            read_split = read.split("_")
-            isolate = read_split[0]
-            isolates.append(isolate)
+            if not cat_reads:
+                subprocess.call("cat" + " " + r1 + " " + r2 + " > " + cat_reads +
+                                " && mash sketch -m 2 " + cat_reads, shell = True)
 
-        count = 0
-        while count < len(isolates):
-            isolate = isolates[count]
-            r1 = fwd_reads[count]
-            r2= rev_reads[count]
-            cat_reads = mash_output_dir + isolate + ".fastq"
-            mash_db = "/opt/genomes/RefSeqSketchesDefaults.msh"
-            mash_sketch = mash_output_dir + isolate + ".fastq.msh"
-            mash_result = mash_output_dir + isolate + "_distance.tab"
+            if not mash_result:
+                subprocess.call("mash dist" + " " + mash_db + " " + mash_sketch + " > " + mash_result, shell = True)
 
-            subprocess.call("cat" + " " + r1 + " " + r2 + " > " + cat_reads +
-                            " && mash sketch -m 2 " + cat_reads, shell = True)
-            subprocess.call("mash dist" + " " + mash_db + " " + mash_sketch + " > " + mash_result, shell = True)
             subprocess.call("sort -gk3" + " " + mash_result + " " + "-o" + " " + mash_result, shell = True)
 
-            count= count + 1
+            taxon = subprocess.check_output("head -1 " + mash_result +
+                                    "| sed 's/.*-\.-//' | awk -F '\t' '{print $1}' | "
+                                    "awk -F '_' '{print $1 \"_\" $2}'", shell=True)
 
+            mash_species[id] = taxon.decode('ASCII').rstrip()
+
+        return(mash_species)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage="sb_taxon.py <input> [options]")
-    parser.add_argument("project", type=str, help="path to Project containing read files")
-    parser.add_argument("-reads_dir", default="", type=str, help="path to read files; default = project_dir/raw_read")
+    parser.add_argument("input", type=str, help="path to dir containing read files")
+    parser.add_argument("-o", default="", type=str, help="Name of output_dir")
     parser.add_argument("-mash", action='store_true', help="Perform taxon prediction using MASH against RefSeq db")
 
 
@@ -91,20 +71,15 @@ if __name__ == '__main__':
         parser.exit()
     args = parser.parse_args()
 
-    project = args.project
+    path = args.input
     mash = args.mash
+    output_dir = args.o
 
-    reads_dir = args.reads_dir
-    if len(reads_dir) > 0:
-        pass
-    else:
-        reads_dir =  "/raw_reads/"
+    if not output_dir:
+        output_dir = path
 
-    project = SBTaxon(project, reads_dir)
-    print("Project selected: " + project.name)
+    reads = SBTaxon(path, output_dir)
+    print("Project selected: " + reads.name)
 
-
-    #print(project.name, project.reads)
     if mash:
-        project.sb_mash()
-
+        print(reads.sb_mash())
