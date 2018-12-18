8bf78a14e1212a71eae1ea78207339adecf652e1
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2018-12-18 08:58:39 -0500

    ensure BS Project is populated with read data

--- core/basemount.py
+++ core/basemount.py
@@ -38,21 +38,22 @@ class Basemount:
 
     def copy_reads(self):
         raw_reads_dir = self.out + "/raw_reads/"
+        if not self.reads:
+            raise ValueError("No reads found in %s"%self.path)
+        else:
+            if not os.path.isdir(raw_reads_dir):
+                os.makedirs(raw_reads_dir)
+                print("Directory made for raw read files: " + raw_reads_dir)
 
-        if not os.path.isdir(raw_reads_dir):
-            os.makedirs(raw_reads_dir)
-            print("Directory made for raw read files: " + raw_reads_dir)
+            for read in self.reads:
+                dest = raw_reads_dir + re.sub('S\d+_L\d+_R', "", os.path.basename(read))
+                dest = dest.replace("_001","")
 
-        for read in self.reads:
-            dest = raw_reads_dir + re.sub('S\d+_L\d+_R', "", os.path.basename(read))
-            dest = dest.replace("_001","")
-
-            if os.path.isfile((dest)):
-                pass
-            else:
-                print("Copying " + read + " to: " + dest)
-                shutil.copyfile(read, dest)
-
+                if os.path.isfile((dest)):
+                    pass
+                else:
+                    print("Copying " + read + " to: " + dest)
+                    shutil.copyfile(read, dest)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage="fileparser.py <input> [options]")
     parser.add_argument("input", type=str, help="Path to fastq files.")
