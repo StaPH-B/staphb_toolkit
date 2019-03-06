c2a6eb7ce69e1510f96172fd149ad661adf5aeb4
Nick Florek <nicholas.florek@slh.wisc.edu>
2019-03-06 14:31:47 -0600

    updated for better stdout and stderr reporting

--- core/calldocker.py
+++ core/calldocker.py
@@ -11,13 +11,13 @@ import json
 import shlex
 
 def call(container,command,cwd='',paths={},remove=True):
-    #access docker environment
+    ###access docker environment
     client = docker.from_env()
 
-    #get the effectie user and group id's
+    ###get the effectie user and group id's
     user = str(os.geteuid())+':'+str(os.getegid())
 
-    #setup mount point paths
+    ###setup mount point paths
     #{"/path/outside":"/path/incontainer"}
     volumes = {}
     if paths:
@@ -25,15 +25,28 @@ def call(container,command,cwd='',paths={},remove=True):
             #TODO add more options than only read/write
             volumes[key] = {'bind':paths[key],'mode':'rw'}
 
-    #run the container
-    client.containers.run(container,command,user=user,volumes=volumes,working_dir=cwd,remove=remove)
+    ###run the container
+    #create empty variable for holding byte object output for the container logs
+    output = b''
+    #try block to run the container
+    try:
+        container_obj = client.containers.run(container,command,user=user,volumes=volumes,working_dir=cwd,remove=remove,detach=True)
+    except:
+        #loop through output as it is streamed
+        for line in container_obj.logs(stream=True):
+            output += line
+    else:
+        for line in container_obj.logs(stream=True):
+            output += line
+    #once container is finished return output as a string
+    return output.decode('utf-8')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage="calldocker.py <docker_container> <command> [options]")
     parser.add_argument("docker_container", type=str, help="Name of the docker container to run")
     parser.add_argument("command", type=str, help="Command to execute in the docker container")
     parser.add_argument("-d", default="", type=str, help="Path to working directory in container")
-    parser.add_argument("-p", default="", type=json.loads, help='Dictonary of paths to mount in the container e.x. {"/path/outside":"/path/incontainer"} note: the quoting')
+    parser.add_argument("-p", default="", type=str, help='Dictonary of paths to mount in the container e.x. \'{"/path/outside":"/path/incontainer"}\' note: the quoting')
     parser.add_argument('-k', dest='k', default=True, action='store_false',help="Keep the container after the container has finished")
 
     if len(sys.argv[1:]) == 0:
@@ -47,7 +60,18 @@ if __name__ == '__main__':
     remove = args.k
     paths = ''
 
-    if args.p:
-        paths = args.p
+    if not cwd:
+        cwd = os.getcwd()
 
-    call(container_name,cmd,cwd=cwd,paths=paths,remove=remove)
+    #check for path argument if empty set a default
+    if args.p:
+        try:
+            paths = json.loads(args.p)
+        except:
+            print("Path incorrectly formatted.")
+            sys.exit()
+    else:
+        paths = {cwd:"/data"}
+
+    #run the container and display the output
+    print(call(container_name,cmd,cwd=cwd,paths=paths,remove=remove))
