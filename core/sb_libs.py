4affcaad7f547dc3bf52e3b48e91d437e59c14af
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2019-03-27 17:38:08 -0400

    parse json for docker tag

--- core/sb_libs.py
+++ core/sb_libs.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-import shutil
+import json
 import argparse
 import re
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
@@ -14,45 +14,27 @@ from staphB_ToolKit.core import calldocker
 
 
 class SB_lib:
-    # path to fastq files
-    path = None
-    # output directory
-    output_dir = None
+    def __init__(self, parameters=None, path=None, docker_image=None, executable=None):
+        self.path=path
+        self.docker_image = docker_image
+        self.executable = executable
+        docker_config = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))[:-4] + "/core/docker_config.json"
+        docker_tag = ""
 
-    def __init__(self, output_dir = None, params=None):
-        if output_dir:
-            self.output_dir = os.path.abspath(output_dir)
-        else:
-            self.output_dir = os.getcwd()
+        with open(docker_config) as config_file:
+            config_file = json.load(config_file)
+            for image in config_file['images']:
+                if image['name'] == docker_image:
+                    docker_tag = image['tag']
 
-        if params:
-            self.params = params
+        self.docker_tag = docker_tag
+        self.parameters = parameters
 
-    def run_docker(self):
+    def run_lib(self):
         # create paths for data
         mounting = {self.path: '/data'}
         out_dir = '/data'
         in_dir = '/data'
 
-        command = ""
-
-        calldocker.call("staphb/{image}".format(image=image), command, '/dataout',mounting)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(usage="mash.py <input> [options]")
-    parser.add_argument("input", type=str, nargs='?', help="path to dir containing read files")
-    parser.add_argument("-o", default="", nargs='?', type=str, help="Name of output_dir")
-
-    if len(sys.argv[1:]) == 0:
-        parser.print_help()
-        parser.exit()
-    args = parser.parse_args()
-
-    path = os.path.abspath(args.input)
-    output_dir = args.o
-
-    if not output_dir:
-        output_dir = os.getcwd()
-
-    lib_obj = SB_lib(path=path,output_dir=output_dir)
-    lib_obj.run_docker()
+        command = f"{self.executable} {self.parameters}"
+        print(calldocker.call(f"staphb/{self.docker_image}:{self.docker_tag}", command, '/dataout', mounting))
