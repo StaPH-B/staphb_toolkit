a19a4e7e8dccd7864c33d02c266108178edd0e37
Kelsey Florek <kelseyflorek22@gmail.com>
2019-08-26 16:34:08 -0500

    added multiprocessing

--- core/sb_libs_multi.py
+++ core/sb_libs_multi.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import json
+import multiprocessing as mp
 from shutil import which
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
 
@@ -17,20 +18,18 @@ else:
 
 ##define and set signal handler, to handle detached containers
 def handler(sig,frame):
-    print('\nShutting down the running docker containers and exiting...')
-    client = docker.from_env()
-    container_list = client.containers.list(filters={"label":"prog=sb_toolkit"})
-    for container in container_list:
-        print("shutting down container: ",container.name)
-        container.kill()
+    #shutdown containers
+    container_engine.shutdown()
     sys.exit()
+
+##attach signal handler
 signal.signal(signal.SIGINT, handler)
 
-class SB_lib:
-    def __init__(self, parameters=None, path=None, docker_image=None, executable=None):
-        self.path=path
+class SB_lib_multi:
+    def __init__(self, command_list=None, path=None, docker_image=None):
+        self.path = path
         self.docker_image = docker_image
-        self.executable = executable
+        self.command_list = command_list
         # TODO: Find a better way to grab json file
         docker_config = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))[:-4] + "/core/docker_config.json"
         docker_tag = ""
@@ -44,6 +43,18 @@ class SB_lib:
         self.docker_tag = docker_tag
         self.parameters = parameters
 
-    def run_lib(self):
-        command = f"{self.executable} {self.parameters}"
-        print(container_engine.call(f"staphb/{self.docker_image}:{self.docker_tag}", command, '/data', self.path))
+    def run_lib(self,jobs):
+        #create multiprocessing pool
+        pool = mp.Pool(processes=jobs)
+
+        #set signal handling to default during spawning of subthreads
+        signal.signal(signal.SIGINT,signal.SIG_DFL)
+
+        results = pool.starmap_async(cd.call,[[f"staphb/{self.docker_image}:{self.docker_tag}",cmd,'/data',self.path] for cmd in self.command_list])
+
+        #reset signal handling to handle shutting down containers
+        signal.signal(signal.SIGINT, handler)
+
+        stdouts = results.get()
+        for stdout in stdouts:
+            print(stdout)
