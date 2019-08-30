aa2b3075fb4d62cd6cbda8cba282d3311bf6a864
Kelsey Florek <nickflorek@gmail.com>
2019-08-30 10:41:36 -0500

    no reason for these to be run alone

--- core/calldocker.py
+++ core/calldocker.py
@@ -60,38 +60,3 @@ def call(container,command,cwd='',paths={},remove=True):
             output += line
     #once container is finished return output as a string
     return output.decode('utf-8')
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(usage="calldocker.py <docker_container> <command> [options]")
-    parser.add_argument("docker_container", type=str, help="Name of the docker container to run")
-    parser.add_argument("command", type=str, help="Command to execute in the docker container")
-    parser.add_argument("-d", default="", type=str, help="Path to working directory in container")
-    parser.add_argument("-p", default="", type=str, help='Dictonary of paths to mount in the container e.x. \'{"/path/outside":"/path/incontainer"}\' note: the quoting')
-    parser.add_argument('-k', dest='k', default=True, action='store_false',help="Keep the container after the container has finished")
-
-    if len(sys.argv[1:]) == 0:
-        parser.print_help()
-        parser.exit()
-    args = parser.parse_args()
-
-    container_name = args.docker_container
-    cmd = args.command
-    cwd = args.d
-    remove = args.k
-    paths = ''
-
-    if not cwd:
-        cwd = os.getcwd()
-
-    #check for path argument if empty set a default
-    if args.p:
-        try:
-            paths = json.loads(args.p)
-        except:
-            print("Path incorrectly formatted.")
-            sys.exit()
-    else:
-        paths = {cwd:"/data"}
-
-    #run the container and display the output
-    print(call(container_name,cmd,cwd=cwd,paths=paths,remove=remove))
