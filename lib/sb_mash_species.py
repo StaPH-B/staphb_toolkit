c4f950461264eee448a9a2a0437d9eccd3c0d61a
Kelsey Florek <kelseyflorek22@gmail.com>
2019-09-04 10:32:11 -0500

    modified mash species to work with singularity and added it to staphb_toolkit

--- lib/sb_mash_species.py
+++ lib/sb_mash_species.py
@@ -36,51 +36,68 @@ class MashSpecies():
         else:
             self.db = "/db/RefSeqSketchesDefaults.msh"
 
-        self.mash_out_dir = self.output_dir + "/mash_output/"
+        self.mash_out_dir = os.path.join(self.output_dir,"mash_output")
 
 
-    def main(self):
+    def run(self):
         #Run MASH to get distances
         mash_species = {}
         print("Performing taxonomic predictions with MASH. . .")
-        for read in self.runfiles.reads:
-            id = self.runfiles.reads[read].id
+        #dictonary of each set of reads found
+        reads_dict = self.runfiles.id_dict()
 
+        for id in reads_dict:
             if os.path.isdir(self.path + "/AppResults"):
                 self.path = self.output_dir
 
-            fwd = os.path.abspath(self.runfiles.reads[read].fwd).replace(self.path, "")
-            rev = os.path.abspath(self.runfiles.reads[read].rev).replace(self.path,"")
+            fwd_path = os.path.basename(reads_dict[id].fwd)
+            rev_path = os.path.basename(reads_dict[id].rev)
 
+            #create mash output file
+            pathlib.Path(os.path.join(self.output_dir,"mash_output")).mkdir(parents=True, exist_ok=True)
+
+            #docker mounting dictonary
+            mash_mounting = {self.path: '/datain', os.path.join(self.output_dir,"mash_output"):'/dataout'}
+
+            #mash result and sketch file name
             mash_result="{id}_distance.tab".format(id=id)
-            pathlib.Path(self.output_dir + "/mash_output/").mkdir(parents=True, exist_ok=True)
-            mash_mounting = {self.path: '/datain', self.output_dir + "/mash_output/":'/dataout'}
-            out_dir = '/dataout'
-            in_dir = '/datain'
+            sketch_name = id+"_sketch"
 
-            mash_sketch_command="bash -c 'mkdir -p {out_dir}/{id}/ && cat {in_dir}/{fwd} {in_dir}/{rev} | mash sketch -r -m 2 "\
-                        "-o {out_dir}/{id}/{sketch} -'".format(id=id, in_dir=in_dir, out_dir=out_dir,
-                                                               sketch=id+"_sketch", fwd=fwd, rev=rev)
-            mash_dist_command="bash -c 'mash dist /db/{db} {out_dir}/{id}/{sketch} > {out_dir}/{id}/{mash_result}'".format(
-                id=id,in_dir=in_dir,out_dir=out_dir, sketch=id+ "_sketch.msh", mash_result=mash_result,
-                db="RefSeqSketchesDefaults.msh")
+            #command for creating the mash sketch
+            mash_sketch_command = f"bash -c 'mkdir -p /dataout/{id} && mash sketch -r -m 2 -o /dataout/{id}/{sketch_name} /datain/{fwd_path} /datain/{rev_path}'"
+
+            #command for running mash distance
+            sketch_name = id+"_sketch.msh"
+            db = "RefSeqSketchesDefaults.msh"
+            mash_dist_command = f"bash -c 'mash dist /db/{db} /dataout/{id}/{sketch_name} > /dataout/{id}/{mash_result}'"
 
             print(id)
-            if not os.path.isfile(self.output_dir + "/mash_output/" + id + "/" + id+"_sketch.msh"):
+            #create and run mash sketch object if it doesn't already exist
+            if not os.path.isfile(os.path.join(*[self.output_dir,"mash_output",id, id,"_sketch.msh"])):
+                #create mash sketch object
                 mash_sketch = sb_programs.Run(command=mash_sketch_command, path=mash_mounting, docker_image="mash")
+                #run mash sketch
                 mash_sketch.run()
 
-            if not os.path.isfile(self.output_dir + "/mash_output/" + id + "/" + mash_result):
+            #create and run mash dist object if it doesn't already exist
+            if not os.path.isfile(os.path.join(*[self.output_dir,"mash_output",id,mash_result])):
+                #create mash distance object
                 mash_dist = sb_programs.Run(command=mash_dist_command, path=mash_mounting, docker_image="mash")
+                #run mash distance
                 mash_dist.run()
-            mash_result_sorted = self.mash_out_dir + "/" + id + "/" + id + "_sorted_distance.tab"
 
-            mash_hits = open(self.mash_out_dir+"/"+id+"/"+mash_result, 'r').readlines()
-            output = open(mash_result_sorted, 'w')
-            for line in sorted(mash_hits, key=lambda line: line.split()[2]):
-                output.write(line)
-            output.close()
+            #path to sorted distance file
+            mash_result_sorted = os.path.join(*[self.mash_out_dir,id,id+"_sorted_distance.tab"])
 
+            #path to mash hits file
+            mash_hits = open(os.path.join(*[self.mash_out_dir,id,mash_result]), 'r').readlines()
+
+            #write out the hits sorted to a file
+            with open(mash_result_sorted, 'w') as output:
+                for line in sorted(mash_hits, key=lambda line: line.split()[2]):
+                    output.write(line)
+
+            #open the sorted hits and get the top hit
             with open(mash_result_sorted) as file:
                 top_hit = file.readline()
                 top_hit = re.sub(r'.*-\.-', '', top_hit)
@@ -88,30 +105,14 @@ class MashSpecies():
                 top_hit=top_hit[0]
                 top_hit=re.split(r'^([^_]*_[^_]*)(_|\.).*$', top_hit)[1]
 
+
+            #specify the top hit as the species for this id
             mash_species[id] = top_hit
 
-            with open(self.mash_out_dir + "/mash_species.csv", 'w') as f:
-                f.write("Isolate,Predicted Species\n")
-                for key in mash_species.keys():
-                    f.write("%s,%s\n"%(key,mash_species[key]))
+        #write out the results to mash_species.csv
+        with open(os.path.join(self.mash_out_dir,"mash_species.csv"), 'w') as f:
+            f.write("Isolate,Predicted Species\n")
+            for key in mash_species.keys():
+                f.write("%s,%s\n"%(key,mash_species[key]))
 
         return mash_species
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(usage="sb_mash_species.py <input> [options]")
-    parser.add_argument("input", type=str, nargs='?', help="path to dir containing read files")
-    parser.add_argument("-o", default="", nargs='?', type=str, help="Name of output_dir")
-
-    if len(sys.argv[1:]) == 0:
-        parser.print_help()
-        parser.exit()
-    args = parser.parse_args()
-
-    path = os.path.abspath(args.input)
-    output_dir = args.o
-
-    if not output_dir:
-        output_dir = os.getcwd()
-
-    mash_species_obj = MashSpecies(path=path,output_dir=output_dir)
-    mash_species_obj.main()
