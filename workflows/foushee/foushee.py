3345e78961283eef0bf6242dc787f2dd6e812419
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2019-09-18 12:38:27 -0400

    add ksnp3 and snp-dists call

--- workflows/foushee/foushee.py
+++ workflows/foushee/foushee.py
@@ -27,6 +27,13 @@ from core import fileparser
 import xml.etree.ElementTree as ET
 
 def group_by_emm(isolate_qual):
+    '''
+    create data dictionary of emm groups in the format:
+        {emm_groupX: [list of isolates ID'ed as emm_groupX],
+        emm_groupY: [list of isolates ID'ed as emm_groupY],
+        ...
+        }
+    '''
     emm_types=[]
     emm_groups={}
     for id in isolate_qual:
@@ -39,7 +46,22 @@ def group_by_emm(isolate_qual):
             emm_groups[isolate_qual[id]["subspecies_predictions"]].append(id)
     return emm_groups
 
-
+def ksnp3_input_file(emm_groups, out_dir):
+    for group in emm_groups.keys():
+        if len(emm_groups[group]) > 1:
+            group_length=len(emm_groups[group])
+            isolates_in_group=", ".join(emm_groups[group])
+            ksnp3_in_file= os.path.join(out_dir, group, f"{group}_assemblies.txt")
+            pathlib.Path(os.path.dirname(ksnp3_in_file)).mkdir(parents=True, exist_ok=True)
+            print(f"{group_length} isolates identified as {group}: {isolates_in_group}")
+            print(f"Creating ksnp3 input file: {ksnp3_in_file}")
+            with open(ksnp3_in_file, "w") as tsvfile:
+                writer = csv.writer(tsvfile, delimiter='\t')
+                for isolate in emm_groups[group]:
+                    assembly_path = os.path.join(*["/datain", "shovill_output", isolate, "contigs.fa"])
+                    writer.writerow([assembly_path, isolate])
+        else:
+            print("Only one isolate identified as "+ group)
 
 ################################
 #main foushee function
@@ -48,16 +70,71 @@ def foushee(memory,cpus,read_file_path,output_dir="",configuration=""):
     print("First run tredegar")
     isolate_qual=tredegar(memory=memory,cpus=cpus,read_file_path=read_file_path,output_dir=output_dir,configuration=configuration)
 
+    #get the configuration file
+    if configuration:
+        config_file_path = os.path.absolute(configuration)
+    else:
+        #use default
+        config_file_path = os.path.join(os.path.abspath(os.path.dirname(os.path.realpath(__file__))),"foushee_config.json")
+
+    with open(config_file_path) as config_file:
+        ksnp3_config = json.load(config_file)
+    ksnp3_params=ksnp3_config["parameters"]["ksnp3"]
+
+    # create emm_group dict
     emm_groups = group_by_emm(isolate_qual)
 
+    # create ksnp3 directory
+    ksnp3_output = os.path.join(output_dir,"ksnp3_output")
+    pathlib.Path(ksnp3_output).mkdir(parents=True, exist_ok=True)
+
+    # create input file for kSNP3 analysis
+    ksnp3_input_file(emm_groups, out_dir=ksnp3_output)
+
+    # Run ksnp3 & snp_dists for each emm group:
     for group in emm_groups.keys():
-        if len(emm_groups[group]) > 1:
-            print(f"{group}_assemblies.txt")
-            for isolate in emm_groups[group]:
-                assembly_path = os.path.join(*["shovill_output", isolate, "contigs.fa"])
-                print(assembly_path)
-        else:
-            print("Only one isolate identified as "+ group)
+        if len(emm_groups[group]) == 1:
+            continue
+
+        # setup mounting in docker container
+        ksnp3_mounting = {os.path.abspath(output_dir): '/datain', os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/dataout'}
+
+        # generate command to run shovill on the id
+        ksnp3_command = f"kSNP3 -in /datain/ksnp3_output/{group}/{group}_assemblies.txt -outdir /dataout/ {ksnp3_params}"
+
+        # # generate ksnp3 object
+        ksnp3_obj = sb_programs.Run(command=ksnp3_command, path=ksnp3_mounting, docker_image="ksnp3")
+
+        # path for the ksnp3 result file
+        ksnp3_result = os.path.join(*[output_dir, "ksnp3_output", group, "core_SNPs_matrix.fasta"])
+
+        #run the SNP analysis process using ksnp3
+        if not os.path.isfile(ksnp3_result):
+             print(f"Performing SNP analysis for isolates identified as {group}")
+             ksnp3_obj.run()
+
+        # create snp-dists output dir:
+        snp_dists_output = os.path.join(os.path.abspath(output_dir), "snp_dists_output")
+        pathlib.Path(os.path.join(snp_dists_output)).mkdir(parents=True, exist_ok=True)
+
+        # setup mounting in docker container
+        snp_dists_mounting = {os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/datain', snp_dists_output: '/dataout'}
+
+        # generate command to run shovill on the id
+        snp_dists_command = f"bash -c 'snp-dists /datain/core_SNPs_matrix.fasta > /dataout/{group}_snp-distance_matrix.tsv'"
+
+        # # generate snp_dists object
+        snp_dists_obj = sb_programs.Run(command=snp_dists_command, path=snp_dists_mounting, docker_image="snp-dists")
+
+        # path for the snp_dists result file
+        snp_dists_result = os.path.join(*[snp_dists_output, f"{group}_snp-distance_matrix.tsv"])
+
+        #run the SNP analysis process using snp_dists
+        if not os.path.isfile(snp_dists_result):
+             print(f"Performing SNP analysis for isolates identified as {group}")
+             snp_dists_obj.run()
+
+
 
 
 
