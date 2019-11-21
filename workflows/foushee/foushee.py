b2d7b64bcb637a7a64f69d879f4b3a8fe09fded1
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2019-11-21 10:41:36 -0500

    add logging and take hard linking outside of ksnp and snp-dists functions

--- workflows/foushee/foushee.py
+++ workflows/foushee/foushee.py
@@ -7,10 +7,16 @@ import json
 import csv
 import pathlib
 import sys,os
-from shutil import copyfile
+import logging
+import getpass
+import datetime
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+
+# load staphb libaries
 from workflows.tredegar.tredegar import tredegar
 from core import sb_programs
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+from core import fileparser
 
 
 def group_by_emm(isolate_qual):
@@ -41,24 +47,22 @@ def ksnp3_input_file(emm_groups, out_dir):
             isolates_in_group=", ".join(emm_groups[group])
             ksnp3_in_file= os.path.join(out_dir, group, f"{group}_assemblies.txt")
             pathlib.Path(os.path.dirname(ksnp3_in_file)).mkdir(parents=True, exist_ok=True)
-            print(f"{group_length} isolates identified as {group}: {isolates_in_group}")
-            print(f"Creating ksnp3 input file: {ksnp3_in_file}")
+            logging.info(f"{group_length} isolates identified as {group}: {isolates_in_group}")
+            logging.info(f"Creating ksnp3 input file: {ksnp3_in_file}")
             with open(ksnp3_in_file, "w") as tsvfile:
                 writer = csv.writer(tsvfile, delimiter='\t')
                 for isolate in emm_groups[group]:
                     assembly_path = os.path.join(*["/datain", "shovill_output", isolate, "contigs.fa"])
                     writer.writerow([assembly_path, isolate])
         else:
-            print(f"Only one isolate identified as {group}: {''.join(emm_groups[group])}")
+            logging.info(f"Only one isolate identified as {group}: {''.join(emm_groups[group])}")
 
 
-def ref_free_snp(output_dir, group, foushee_config):
-    # path to the ksnp3 result file
-    ksnp3_result = os.path.join(*[output_dir, "ksnp3_output", group, "core_SNPs_matrix.fasta"])
+def ref_free_snp(output_dir, group, foushee_config, ksnp3_matrix):
 
     # run the SNP analysis process using ksnp3 if output does not already exist
-    if not os.path.isfile(ksnp3_result):
-        print(f"Performing SNP analysis for isolates identified as {group}")
+    if not os.path.isfile(ksnp3_matrix):
+        logging.info(f"Performing SNP analysis for isolates identified as {group}")
 
         # setup mounting in docker container
         ksnp3_mounting = {os.path.abspath(output_dir): '/datain',
@@ -73,40 +77,26 @@ def ref_free_snp(output_dir, group, foushee_config):
         ksnp3_obj = sb_programs.Run(command=ksnp3_command, path=ksnp3_mounting, image=ksnp3_configuration["image"], tag=ksnp3_configuration["tag"])
         ksnp3_obj.run()
 
-        # hard link ksnp3 output into foushee directory
-        ksnp3_tree = ksnp3_result.replace("core_SNPS_matrix.fasta", "tree.core.tre")
-        ksnp3_tree_dest = os.path.join(*[output_dir, "foushee_output", f"{group}_core_SNPs_matrix.fasta"])
-        os.link(ksnp3_tree, ksnp3_tree_dest)
 
-
-def snp_matrix(output_dir, group, foushee_config):
-    # path to the snp_dists result file
-    snp_dists_output = os.path.join(os.path.abspath(output_dir), "snp_dists_output")
-    snp_dists_result = os.path.join(*[snp_dists_output, f"{group}_snp_distance_matrix.tsv"])
-
-    #  run the SNP analysis process using snp_dists if output does not already exist
+def snp_matrix(output_dir, group, foushee_config, snp_dists_output_dir, snp_dists_result):
+     #  run the SNP analysis process using snp_dists if output does not already exist
     if not os.path.isfile(snp_dists_result):
-        print(f"Performing SNP analysis for isolates identified as {group}")
+        logging.info(f"Performing SNP analysis for isolates identified as {group}")
 
         # create snp-dists output dir:
-        snp_dists_output = os.path.join(os.path.abspath(output_dir), "snp_dists_output")
-        pathlib.Path(os.path.join(snp_dists_output)).mkdir(parents=True, exist_ok=True)
+        pathlib.Path(os.path.join(snp_dists_output_dir)).mkdir(parents=True, exist_ok=True)
 
         # setup mounting in docker container
-        snp_dists_mounting = {os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/datain',
-                              snp_dists_output: '/dataout'}
+        snp_dists_mounting = {os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/datain', snp_dists_output_dir: '/dataout'}
 
         # generate command to run shovill on the id
         snp_dists_configuration = foushee_config["parameters"]["snp-dists"]
         snp_dists_params = snp_dists_configuration["params"]
-        snp_dists_command = f"bash -c 'snp-dists /datain/core_SNPs_matrix.fasta > /dataout/{group}_snp_distance_matrix.tsv {snp_dists_params}'"
+        snp_dists_command = f"bash -c 'snp-dists /datain/core_SNPs_matrix.fasta > /dataout/{group}_pairwise_snp_distance_matrix.tsv {snp_dists_params}'"
 
         # generate snp_dists object and then run it
         snp_dists_obj = sb_programs.Run(command=snp_dists_command, path=snp_dists_mounting, image=snp_dists_configuration["image"], tag=snp_dists_configuration["tag"])
         snp_dists_obj.run()
-
-        # hard link snp-dists output into foushee directory
-        os.link(snp_dists_result, snp_dists_result.replace("snp_dists_output", "foushee_output"))
 
 ################################
 # main foushee function
@@ -116,13 +106,11 @@ def foushee(memory,cpus,read_file_path,output_dir="",configuration=""):
     #if we don't have an output dir, use the cwd with a tredegar_output dir
     #if we do get the absolute path
     if not output_dir:
-        output_dir = os.path.join(os.getcwd(),"foushee_output")
+        project = f"foushee_run_{datetime.datetime.today().strftime('%Y-%m-%d')}"
+        output_dir = os.path.join(os.getcwd(), project)
     else:
         output_dir = os.path.abspath(output_dir)
-
-
-    print("First run tredegar")
-    isolate_qual=tredegar(memory=memory,cpus=cpus,read_file_path=read_file_path,output_dir=output_dir,configuration=configuration)
+        project = os.path.basename(output_dir)
 
     #get the configuration file
     if configuration:
@@ -137,6 +125,35 @@ def foushee(memory,cpus,read_file_path,output_dir="",configuration=""):
     # create foushee output dir
     foushee_output = os.path.join(output_dir, "foushee_output")
     pathlib.Path(foushee_output).mkdir(parents=True, exist_ok=True)
+
+    # set logging file
+    foushee_log_file = os.path.join(foushee_output, project + "_foushee.log")
+    logFormatter = logging.Formatter("%(asctime)s: %(message)s")
+    rootLogger = logging.getLogger()
+
+    fileHandler = logging.FileHandler(foushee_log_file)
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
+    rootLogger.setLevel(logging.INFO)
+    logging.info(
+        f"{getpass.getuser()} ran Foushee as: foushee(memory={memory}, cpus={cpus}, read_file_path={read_file_path}, output_dir={output_dir}, configuration={configuration})")
+
+    # process the raw reads
+    fastq_files = fileparser.ProcessFastqs(read_file_path, output_dir=output_dir)
+
+    # update configuration object with input files
+    foushee_config = fastq_files.inputSubdomain(foushee_config)
+    foushee_config["execution_info"]["run_id"] = project
+    foushee_config["execution_info"]["user"] = getpass.getuser()
+    foushee_config["execution_info"]["datetime"] = datetime.datetime.today().strftime('%Y-%m-%d')
+
+    logging.info("First run tredegar")
+    isolate_qual=tredegar(memory=memory,cpus=cpus,read_file_path=read_file_path,output_dir=output_dir,configuration=configuration)
 
     # create emm_group dict
     emm_groups = group_by_emm(isolate_qual)
@@ -153,56 +170,32 @@ def foushee(memory,cpus,read_file_path,output_dir="",configuration=""):
         if len(emm_groups[group]) == 1:
             continue
 
-        # # setup mounting in docker container
-        # ksnp3_mounting = {os.path.abspath(output_dir): '/datain', os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/dataout'}
-        #
-        # # generate command to run shovill on the id
-        # ksnp3_command = f"kSNP3 -in /datain/ksnp3_output/{group}/{group}_assemblies.txt -outdir /dataout/ {ksnp3_params}"
-        #
-        # # # generate ksnp3 object
-        # ksnp3_obj = sb_programs.Run(command=ksnp3_command, path=ksnp3_mounting, docker_image="ksnp3")
-        #
-        # # path for the ksnp3 result file
-        # ksnp3_result = os.path.join(*[output_dir, "ksnp3_output", group, "core_SNPs_matrix.fasta"])
-        #
-        # # run the SNP analysis process using ksnp3
-        # if not os.path.isfile(ksnp3_result):
-        #     print(f"Performing SNP analysis for isolates identified as {group}")
-        #     ksnp3_obj.run()
-        #     ksnp3_tree = ksnp3_result.replace("core_SNPS_matrix.fasta", "tree.core.tre")
-        #     ksnp3_tree_dest = os.path.join(*[output_dir, "foushee_output", f"{group}_core_SNPs_matrix.fasta"])
-        #     os.link(ksnp3_tree, ksnp3_tree_dest)
-
         # run reference free SNP calling with ksnp3
-        ref_free_snp(output_dir, group, foushee_config)
+        ksnp3_matrix = os.path.join(*[output_dir, "ksnp3_output", group, "core_SNPs_matrix.fasta"])
+        ref_free_snp(output_dir, group, foushee_config, ksnp3_matrix)
+
+        # hard link ksnp3 output into foushee directory
+        ksnp3_tree = ksnp3_matrix.replace("core_SNPs_matrix.fasta", "tree.core.tre")
+        ksnp3_tree_dest = os.path.join(foushee_output, f"{group}_tree.core.tre")
+        if not os.path.isfile(ksnp3_tree_dest):
+            os.link(ksnp3_tree, ksnp3_tree_dest)
 
         # generate snp distance matrix with snp-dists
-        snp_matrix(output_dir, group, foushee_config)
+        snp_dists_output_dir = os.path.join(os.path.abspath(output_dir), "snp_dists_output")
+        snp_dists_result = os.path.join(*[snp_dists_output_dir, f"{group}_pairwise_snp_distance_matrix.tsv"])
+        snp_matrix(output_dir, group, foushee_config, snp_dists_output_dir, snp_dists_result)
 
-        #
-        # # create snp-dists output dir:
-        # snp_dists_output = os.path.join(os.path.abspath(output_dir), "snp_dists_output")
-        # pathlib.Path(os.path.join(snp_dists_output)).mkdir(parents=True, exist_ok=True)
-        #
-        # # setup mounting in docker container
-        # snp_dists_mounting = {os.path.join(os.path.abspath(output_dir), "ksnp3_output", group): '/datain', snp_dists_output: '/dataout'}
-        #
-        # # generate command to run shovill on the id
-        # snp_dists_command = f"bash -c 'snp-dists /datain/core_SNPs_matrix.fasta > /dataout/{group}_snp_distance_matrix.tsv'"
-        #
-        # # # generate snp_dists object
-        # snp_dists_obj = sb_programs.Run(command=snp_dists_command, path=snp_dists_mounting, docker_image="snp-dists")
-        #
-        # # path for the snp_dists result file
-        # snp_dists_result = os.path.join(*[snp_dists_output, f"{group}_snp_distance_matrix.tsv"])
-        #
-        # #  run the SNP analysis process using snp_dists
-        # if not os.path.isfile(snp_dists_result):
-        #     print(f"Performing SNP analysis for isolates identified as {group}")
-        #     snp_dists_obj.run()
-        #     os.link(snp_dists_result, snp_dists_result.replace("snp_dists_output", "foushee_output"))
+        # hard link snp-dists output into foushee directory
+        snp_dists_dest= os.path.join(foushee_output, f"{group}_pairwise_snp_distance_matrix.tsv")
+        if not os.path.isfile(snp_dists_dest):
+            os.link(snp_dists_result, snp_dists_dest)
 
-        print(f"Done! Hard links for the ksnp3 core tree and snps-dists distance matrix made at {foushee_output}")
+        # update config file to include tredegar report
+        foushee_config["file_io"]["output_files"]["ksnp_core_tree"] = os.path.join(foushee_output, f"{group}_tree.core.tre")
+        foushee_config["file_io"]["output_files"]["pairwise_snps_dists_matrix"] = os.path.join(foushee_output, f"{group}_pairwise_snp_distance_matrix.tsv")
+        foushee_config["file_io"]["output_files"]["log_file"] =foushee_log_file
+
+        logging.info(f"Done! Hard links for the ksnp3 core tree and snps-dists distance matrix made at {foushee_output}")
 
 
 
