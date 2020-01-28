1c636e1425452c5cdffab5c4559a38ab7b3080d9
Kelsey Florek <kelseyflorek22@gmail.com>
2020-01-28 07:27:17 -0600

    add staphb toolkit to path

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import subprocess, os, sys
 
 print("Checking Dependencies...")
 
+toolkit_dir = os.path.dirname(os.path.abspath(__file__))
+
 python_version = ""
 pip_cmd = ""
 container = False
@@ -40,6 +42,18 @@ if not pip_cmd:
 
 if python_version and pip_cmd:
     print("Installing dependencies")
-    requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"requirements.txt")
+    requirements_path = os.path.join(toolkit_dir,"requirements.txt")
     stdout = subprocess.run([pip_cmd,"install","-r",requirements_path],stdout=subprocess.PIPE)
     print(stdout.stdout.decode('utf-8'))
+
+#add to path for bash
+#TODO make this more useful for other shells
+print("Setting up path...")
+stdout = subprocess.run(["which","staphb_toolkit"],stdout=subprocess.PIPE)
+if not stdout.stdout.decode('utf-8'):
+    homedir = os.environ['HOME']
+    bashrc = os.path.join(homedir,".bashrc")
+    with open(bashrc,'a') as outfile:
+        outfile.write("#path for staphb_toolkit\n")
+        outfile.write(f"export PATH=\"{toolkit_dir}:$PATH\"")
+print("Done")
