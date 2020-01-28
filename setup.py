606db4e896f8fb673bdfd73228500a475314675a
Kelsey Florek <kelseyflorek22@gmail.com>
2020-01-27 21:23:42 -0600

    setup bug fix

--- setup.py
+++ setup.py
@@ -7,22 +7,32 @@ python_version = ""
 pip_cmd = ""
 container = False
 #check python version
-stdout = subprocess.run(["python3","-V"],stdout=subprocess.PIPE)
-if "Python 3.7" in stdout.stdout.decode('utf-8'):
-    python_cmd = "3.7"
-else:
-    os.system("which python3")
+try:
+    stdout = subprocess.run(["python3","-V"],stdout=subprocess.PIPE)
+    if "Python 3.7" in stdout.stdout.decode('utf-8'):
+        python_version = "3.7"
+    else:
+        os.system("which python3")
+        print("Check your python installation, python3 must be >= python version 3.7")
+        sys.exit(1)
+except FileNotFoundError:
     print("Check your python installation, python3 must be >= python version 3.7")
     sys.exit(1)
 
 #check pip version
-stdout = subprocess.run(["pip3","-V"],stdout=subprocess.PIPE)
-if "python 3.7" in stdout.stdout.decode('utf-8'):
-    pip_cmd = "pip3"
+try:
+    stdout = subprocess.run(["pip3","-V"],stdout=subprocess.PIPE)
+    if "python 3.7" in stdout.stdout.decode('utf-8'):
+        pip_cmd = "pip3"
+except:
+    pass
 
-stdout = subprocess.run(["pip","-V"],stdout=subprocess.PIPE)
-if "python 3.7" in stdout.stdout.decode('utf-8'):
-    pip_cmd = "pip"
+try:
+    stdout = subprocess.run(["pip","-V"],stdout=subprocess.PIPE)
+    if "python 3.7" in stdout.stdout.decode('utf-8'):
+        pip_cmd = "pip"
+except:
+    pass
 
 if not pip_cmd:
     print("Please verify the python 3.7 pip installation.")
@@ -31,5 +41,5 @@ if not pip_cmd:
 if python_version and pip_cmd:
     print("Installing dependencies")
     requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"requirements.txt")
-    stdout = subprocess.run([pip_cmd,"-r",requirements_path],stdout=subprocess.PIPE)
+    stdout = subprocess.run([pip_cmd,"install","-r",requirements_path],stdout=subprocess.PIPE)
     print(stdout.stdout.decode('utf-8'))
