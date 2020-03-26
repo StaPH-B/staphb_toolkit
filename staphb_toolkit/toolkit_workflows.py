17abd6de83ecce74d43443a2e0b9a8584057c943
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2020-03-26 17:10:14 -0400

    add aws_batch profile

--- staphb_toolkit/toolkit_workflows.py
+++ staphb_toolkit/toolkit_workflows.py
@@ -155,18 +155,22 @@ def main():
         #check for user profile
         if args.profile:
             profile = args.profile
+            
+        #set work dir into local logs dir if profile not aws
+        work = ""
+        if profile != "aws":
+            work = f"-w {args.output}/logs/work"
 
         #build command
         command = nextflow_path
-        command = command + f" {monroe_path} -profile {profile} -resume --reads {args.reads_path} --primers {args.primers} --outdir {args.output} -with-trace {args.output}/logs/Monroe_trace.txt -with-report {args.output}/logs/Monroe_execution_report.html -w {args.output}/logs/work"
-
+        command = command + f" {monroe_path} -profile {profile} -resume --reads {args.reads_path} --primers {args.primers} --outdir {args.output} -with-trace {args.output}/logs/Monroe_trace.txt -with-report {args.output}/logs/Monroe_execution_report.html {work}"
         #run command using nextflow in a subprocess
         print("Starting the Monroe pipeline:")
         try:
             process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
             nextflow_printer(process)
         except KeyboardInterrupt:
-            print("Quitting Tredegar...")
+            print("Quitting Monroe...")
             process.terminate()
             print("Done.")
             sys.exit(1)
