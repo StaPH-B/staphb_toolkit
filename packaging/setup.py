1d02361eb35e424420d22239a2523c39578a784b
Kelsey Florek <kelseyflorek22@gmail.com>
2020-05-18 16:31:01 -0500

    1.0.4 release

--- packaging/setup.py
+++ packaging/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="staphb_toolkit",
-    version="1.0.3",
+    version="1.0.4",
     author="Kelsey Florek, Kevin Libuit",
     author_email="kelsey.florek@slh.wisc.edu, kevin.libuit@dgs.virginia.gov",
     description="A ToolKit of commonly used Public Health Bioinformatics Tools",
